e40fd65e0ae5e98c920db6d5def8004155f6406a
Youssef <youssefbayoumi72@gmail.com>
2026-05-18 22:01:10 +0300

    smtp issue

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,9 @@ const jwt = require("jsonwebtoken");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
